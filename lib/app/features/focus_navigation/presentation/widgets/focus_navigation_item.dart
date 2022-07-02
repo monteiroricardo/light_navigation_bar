21ef04f69a8ba693f44a0cd748be35eb5f5b0caa
monteiro <ricardohenrique071@gmail.com>
2022-07-02 17:28:06 -0300

    feat: init focus navigation item

--- lib/app/features/focus_navigation/presentation/widgets/focus_navigation_item.dart
+++ lib/app/features/focus_navigation/presentation/widgets/focus_navigation_item.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:focus_navigation/app/features/focus_navigation/presentation/style/focus_navigation_style.dart';
 
+class FocusNavigationItem extends StatefulWidget {
+  final VoidCallback onTap;
+  final IconData activeIcon;
+  final IconData inactiveIcon;
+  final bool isActive;
+  final FocusNavigationStyle focusNavigationStyle;
+
+  const FocusNavigationItem(
+      {super.key,
+      required this.onTap,
+      required this.activeIcon,
+      required this.inactiveIcon,
+      required this.isActive,
+      required this.focusNavigationStyle});
+
+  @override
+  State<FocusNavigationItem> createState() => _FocusNavigationItemState();
+}
+
+class _FocusNavigationItemState extends State<FocusNavigationItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
